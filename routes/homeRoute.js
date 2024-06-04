00b59670e73611ff63cc9cf261489f5a74443173
= <=>
2024-06-05 00:03:44 +0530

    updated code

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -6,4 +6,10 @@ router.get('/', async (req, res, next) => {
     res.render('home');
 })
 
+router.get('/ui', async (req, res, next) => {
+    res.render('course-single');
+})
+router.get('/ub', async (req, res, next) => {
+    res.render('index');
+})
 module.exports = router;
