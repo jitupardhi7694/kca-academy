558033042837f84cf02de29b3202d383611d187f
= <=>
2024-06-12 23:53:19 +0530

    updated code

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,16 +3,19 @@ const router = express.Router();
 
 
 router.get('/', async (req, res, next) => {
-    res.render('home');
+    await res.render('home');
 })
 
 router.get('/about-kcc', async (req, res, next) => {
-    res.render('about-kcc');
+    await res.render('about-kcc');
+})
+router.get('/kcc-answer', async (req, res, next) => {
+    await res.render('student_help');
 })
 router.get('/ui', async (req, res, next) => {
-    res.render('course-single');
+    await res.render('course-single');
 })
-router.get('/ub', async (req, res, next) => {
-    res.render('index');
+router.get('/login', async (req, res, next) => {
+    await res.render('admin/login');
 })
 module.exports = router;
